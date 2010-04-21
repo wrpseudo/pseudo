f8410162d1bd5b61c05434669e20a160379488de
Peter Seebach <peter.seebach@windriver.com>
2010-04-21 11:15:45 -0700

    Remove obsolete symlink stuff from fxstatat.

--- guts/__fxstatat64.c
+++ guts/__fxstatat64.c
@@ -8,7 +8,6 @@
  */
 	pseudo_msg_t *msg;
 	int save_errno;
-	mode_t save_mode = 0;
 
 #ifdef PSEUDO_NO_REAL_AT_FUNCTIONS
 	if (dirfd != AT_FDCWD) {
@@ -16,11 +15,6 @@
 		return -1;
 	}
 #endif
-	/* If the file is actually a symlink, we grab the db values
-	 * for the underlying target, then mask in the size and mode
-	 * from the link.  Otherwise, we just use the db values (if
-	 * any).
-	 */ 
 	if (flags & AT_SYMLINK_NOFOLLOW) {
 #ifdef PSEUDO_NO_REAL_AT_FUNCTIONS
 		rc = real___lxstat64(ver, path, buf);
@@ -29,10 +23,6 @@
 #endif
 		if (rc == -1) {
 			return rc;
-		}
-		/* it's a symlink, stash its mode */
-		if (S_ISLNK(buf->st_mode)) {
-			save_mode = buf->st_mode;
 		}
 	} else {
 #ifdef PSEUDO_NO_REAL_AT_FUNCTIONS
@@ -44,9 +34,6 @@
 			return rc;
 		}
 	}
-	/* if we got here:  we have valid stat data, for either the symlink
-	 * (if it is a symlink, and we have NOFOLLOW on) or the target.
-	 */
 	save_errno = errno;
 
 	if (ver != _STAT_VER) {
@@ -62,9 +49,6 @@
 	msg = pseudo_client_op(OP_STAT, 0, -1, dirfd, path, buf);
 	if (msg) {
 		pseudo_stat_msg(buf, msg);
-		if (save_mode) {
-			buf->st_mode = save_mode;
-		}
 	}
 
 	errno = save_errno;
