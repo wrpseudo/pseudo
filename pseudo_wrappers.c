cc00abcd92f3d1c491d262266115dcee71b9a7a1
Peter Seebach <peter.seebach@windriver.com>
2010-04-21 13:34:33 -0700

    Allow specification of debug output file.
    This includes allowing %s (program name) and %d (pid).

--- pseudo_wrappers.c
+++ pseudo_wrappers.c
@@ -121,6 +121,7 @@ pseudo_populate_wrappers(void) {
 	char *debug;
 	static int done = 0;
 	char *pseudo_path = 0;
+	int fd;
 
 	if (done)
 		return done;
@@ -147,6 +148,29 @@ pseudo_populate_wrappers(void) {
 		int level = atoi(debug);
 		for (i = 0; i < level; ++i) {
 			pseudo_debug_verbose();
+		}
+	}
+	if (getenv("PSEUDO_DEBUG_FILE")) {
+		char filebuf[pseudo_path_max()], *fmt = getenv("PSEUDO_DEBUG_FILE");
+		char *name, *pid;
+		name = strstr(fmt, "%s");
+		pid = strstr(fmt, "%d");
+		if (name && pid) {
+			if (name > pid) {
+				snprintf(filebuf, pseudo_path_max(), fmt, getpid(), program_invocation_short_name);
+			} else {
+				snprintf(filebuf, pseudo_path_max(), fmt, program_invocation_short_name, getpid());
+			}
+		} else if (name) {
+			snprintf(filebuf, pseudo_path_max(), fmt, program_invocation_short_name);
+		} else if (pid) {
+			snprintf(filebuf, pseudo_path_max(), fmt, getpid());
+		} else {
+			snprintf(filebuf, pseudo_path_max(), "%s", fmt);
+		}
+		fd = open(filebuf, O_WRONLY | O_APPEND | O_CREAT, 0644);
+		if (fd >= 0) {
+			pseudo_util_debug_fd = fd;
 		}
 	}
 	/* must happen after wrappers are set up, because it can call
