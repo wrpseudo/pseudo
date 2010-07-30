7c9033fc63bd40e9cf0562ad5322274c08d3b39f
Konrad Scherer <Konrad.Scherer@windriver.com>
2010-07-30 13:08:31 -0400

    Add basic test harness to test pseudo
    CQ: WIND00225366
    
    A simple test harness that runs all the scripts in the test
    directory. Each test script is run inside pseudo and uses exit status
    to report back whether the test was successful. The script uses the
    build executables, not the installed ones and each test run starts
    with an empty database.

---
+++ run_tests.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+opt_verbose=
+
+usage()
+{
+    echo >&2 "usage:"
+    echo >&2 "  run_tests [-v|--verbose]"
+    exit 1
+}
+
+for arg
+do
+        case $arg in
+        --)     shift; break ;;
+        -v | --verbose)
+                opt_verbose=-v
+                ;;
+        *)
+                usage
+                ;;
+        esac
+done
+
+#The tests will be run on the build dir, not the installed versions
+#This requires to following be set properly.
+export PSEUDO_PREFIX=${PWD}
+export LD_LIBRARY_PATH=".:${LD_LIBRARY_PATH}"
+
+num_tests=0
+num_passed_tests=0
+
+for file in test/test*.sh
+do
+    filename=${file#test/}
+    let num_tests++
+    mkdir -p var/pseudo
+    ./bin/pseudo $file ${opt_verbose}
+    if [ "$?" -eq "0" ]; then
+        let num_passed_tests++
+        if [ "${opt_verbose}" == "-v" ]; then
+            echo "${filename%.sh}: Passed."
+        fi
+    else
+        echo "${filename/%.sh}: Failed."
+    fi
+    rm -rf var
+done
+echo "${num_passed_tests}/${num_tests} test(s) passed."
+
---
+++ test/test-dir-move.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+mkdir d1
+touch d1/f1
+mv d1 d2
+fileuid=`\ls -n1 d2/f1 | awk '{ print $3 }'`
+if [ "$fileuid" == "$UID" ]
+then
+    #echo "Passed."
+    rm -rf d2
+    exit 0
+fi
+rm -rf d2
+#echo "Failed"
+exit 1
