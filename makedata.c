a4cf5f2f5bac9aeb1d9aac73140411b7eb69ee00
Peter Seebach <peter.seebach@windriver.com>
2011-02-01 13:07:03 -0600

    Add performance test (rough draft).

---
+++ makedata.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int
+main(void) {
+	int i, j, k, l;
+	/* templateish form used so it's obvious that it's long enough */
+	char name[] = "dir_%d/dir_%d/%d%d.txt";
+	for (i = 0; i < 10; ++i) {
+		snprintf(name, sizeof(name), "dir_%d", i);
+		mkdir(name);
+		for (j = 0; j < 40; ++j) {
+			snprintf(name, sizeof(name), "dir_%d/dir_%d", i, j);
+			mkdir(name);
+			for (k = 0; k < 10; ++k) {
+				for (l = 0; l < 10; ++l) {
+					FILE *fp;
+					snprintf(name, sizeof(name),
+						"dir_%d/dir_%d/%d%d.txt",
+						i, j, k, l);
+					fp = fopen(name, "w");
+					if (fp) {
+						fprintf(fp, "dummy file.\n");
+						fclose(fp);
+					}
+				}
+			}
+		}
+	}
+	return 0;
+}
