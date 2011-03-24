a15f432fd27d3a061369dca724d541394be20d49
Seebs <seebs@seebs.net>
2011-03-24 15:54:39 -0500

    Woo!  Last obvious error fixed.

--- ports/darwin/guts/flistxattr.c
+++ ports/darwin/guts/flistxattr.c
@@ -2,11 +2,11 @@
  * Copyright (c) 2011 Wind River Systems; see
  * guts/COPYRIGHT for information.
  *
- * ssize_t flistxattr(int filedes, char *list, size_t size)
+ * ssize_t flistxattr(int filedes, char *list, size_t size, int options)
  *	ssize_t rc = -1;
  */
 
-	rc = real_flistxattr(filedes, list, size);
+	rc = real_flistxattr(filedes, list, size, options);
 
 /*	return rc;
  * }
--- ports/darwin/guts/setxattr.c
+++ ports/darwin/guts/setxattr.c
@@ -2,11 +2,11 @@
  * Copyright (c) 2011 Wind River Systems; see
  * guts/COPYRIGHT for information.
  *
- * int setxattr(const char *pathname, const char *name, const void *value, size_t size, int flags)
+ * int setxattr(const char *pathname, const char *name, const void *value, size_t size, u_int32_t position, int options)
  *	int rc = -1;
  */
 
-	rc = real_setxattr(pathname, name, value, size, flags);
+	rc = real_setxattr(pathname, name, value, size, position, options);
 
 /*	return rc;
  * }
