3532684e2f4c8a4e9086365adcda4079d20f0a92
Peter Seebach <peter.seebach@windriver.com>
2012-02-06 13:47:09 -0600

    The O_LARGEFILE value was getting merged into mode (where it was ignored) rather than flags (where it was needed), meaning that the open64 type functions didn't work as intended on 32-bit hosts.
    Conflicts:
    
    	ChangeLog.txt
    
    (Merged from master branch.)

--- ports/linux/guts/open64.c
+++ ports/linux/guts/open64.c
@@ -1,5 +1,5 @@
 /* 
- * Copyright (c) 2008-2010 Wind River Systems; see
+ * Copyright (c) 2008-2010,2012 Wind River Systems; see
  * guts/COPYRIGHT for information.
  *
  * static int
@@ -7,7 +7,7 @@
  *	int rc = -1;
  */
 
-	rc = wrap_openat(AT_FDCWD, path, flags, mode | O_LARGEFILE);
+	rc = wrap_openat(AT_FDCWD, path, flags | O_LARGEFILE, mode);
 
 /*	return rc;
  * }
