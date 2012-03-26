29ee281563a27bdbfa00613f20a4aaa5ca2a42ae
Peter Seebach <peter.seebach@windriver.com>
2012-03-26 15:53:10 -0500

    add popen() call
    We weren't trapping popen(), so if environment variables were in an
    inconsistent state when popen() was called, Bad Things Happened.  Add
    a popen() wrapper.  Like a couple of other special cases, is applied
    even when pseudo is theoretically disabled, and that includes the antimagic
    case.  (But we never use popen() so that's fine.)

---
+++ ports/unix/guts/popen.c
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2012 Wind River Systems; see
+ * guts/COPYRIGHT for information.
+ *
+ * FILE *popen(const char *command, const char *mode)
+ *	FILE *rc = NULL;
+ */
+
+	/* on at least some systems, popen() calls fork and exec
+	 * in ways that avoid our usual enforcement of the environment.
+	 */
+	pseudo_setupenv();
+	if (pseudo_get_value("PSEUDO_UNLOAD"))
+		pseudo_dropenv();
+
+	rc = real_popen(command, mode);
+
+/*	return rc;
+ * }
+ */
---
+++ ports/unix/pseudo_wrappers.c
@@ -0,0 +1,50 @@
+FILE *
+popen(const char *command, const char *mode) {
+	sigset_t saved;
+	
+	FILE *rc = NULL;
+
+	if (!pseudo_check_wrappers() || !real_popen) {
+		/* rc was initialized to the "failure" value */
+		pseudo_enosys("popen");
+		return rc;
+	}
+
+	pseudo_debug(4, "called: popen\n");
+	pseudo_sigblock(&saved);
+	if (pseudo_getlock()) {
+		errno = EBUSY;
+		sigprocmask(SIG_SETMASK, &saved, NULL);
+		return NULL;
+	}
+
+	int save_errno;
+	/* exec*() use this to restore the sig mask */
+	pseudo_saved_sigmask = saved;
+	rc = wrap_popen(command, mode);
+	
+	save_errno = errno;
+	pseudo_droplock();
+	sigprocmask(SIG_SETMASK, &saved, NULL);
+#if 0
+/* This can cause hangs on some recentish systems which use locale
+ * stuff for strerror...
+ */
+	pseudo_debug(4, "completed: popen (maybe: %s)\n", strerror(save_errno));
+#endif
+	pseudo_debug(4, "completed: popen (errno: %d)\n", save_errno);
+	errno = save_errno;
+	return rc;
+}
+
+static FILE *
+wrap_popen(const char *command, const char *mode) {
+	FILE *rc = NULL;
+	
+	
+
+#include "guts/popen.c"
+
+	return rc;
+}
+
