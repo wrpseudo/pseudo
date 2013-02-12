363a94bb851046f62648d7c96c749e899bd0648e
Peter Seebach <peter.seebach@windriver.com>
2013-02-12 15:42:49 -0600

    Fix link() to work when chrooted
    The automatic path fixups invoked for names which end in the string
    "path" was still applying to link(), which then called linkat(),
    which would do the same path fixups; if you were chrooted, this would
    produce bogus paths. On systems which actually have linkat(), this
    would produce the even more mysterious behavior that the link would
    succeed, but the following stat would fail.
    
    Solution: Change the wrapfuncs prototypes for link() so it doesn't
    invoke automatic path name fixups.

--- ports/unix/guts/link.c
+++ ports/unix/guts/link.c
@@ -1,9 +1,9 @@
 /* 
- * Copyright (c) 2008-2010, 2012 Wind River Systems; see
+ * Copyright (c) 2008-2010, 2012, 2013 Wind River Systems; see
  * guts/COPYRIGHT for information.
  *
  * static int
- * wrap_link(const char *oldpath, const char *newpath) {
+ * wrap_link(const char *oldname, const char *newname) {
  *	int rc = -1;
  */
         /* since 2.6.18 or so, linkat supports AT_SYMLINK_FOLLOW, which
@@ -12,7 +12,7 @@
          * does NOT support AT_SYMLINK_NOFOLLOW! So define this in
          * your port's portdefs.h or hope the default works for you.
          */
-        rc = wrap_linkat(AT_FDCWD, oldpath, AT_FDCWD, newpath,
+        rc = wrap_linkat(AT_FDCWD, oldname, AT_FDCWD, newname,
                          PSEUDO_LINK_SYMLINK_BEHAVIOR);
 
 /*	return rc;
