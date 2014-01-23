a6fe8e6d09f3579f5a967aac9e8cb2f212136eb7
Peter Seebach <peter.seebach@windriver.com>
2014-01-23 15:11:31 -0600

    mkdir was storing/reporting modes with 0700 added

--- ports/unix/guts/mkdirat.c
+++ ports/unix/guts/mkdirat.c
@@ -25,6 +25,7 @@
 		stat_rc = base_fstatat(dirfd, path, &buf, AT_SYMLINK_NOFOLLOW);
 #endif
 		if (stat_rc != -1) {
+			buf.st_mode = PSEUDO_DB_MODE(buf.st_mode, mode);
 			pseudo_client_op(OP_MKDIR, 0, -1, dirfd, path, &buf);
 		} else {
 			pseudo_debug(PDBGF_OP, "mkdir of %s succeeded, but stat failed: %s\n",
