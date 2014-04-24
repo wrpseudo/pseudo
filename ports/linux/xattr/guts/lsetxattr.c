f7fccc4baf8b2105d7b832fdeb6c2eb28ace70be
Peter Seebach <peter.seebach@windriver.com>
2014-04-24 16:13:36 -0500

    Spell flag override correctly, rename flags
    The "/* flags = AT_SYMLINK_NOFOLLOW */" comment only works if
    it comes AFTER the semicolon in wrapfuncs.in. Who knew? Fix
    those. Also rename the "flags" arguments for *setxattr() to
    "xflags" to avoid any confusion about the flags variable.
    
    Signed-off-by: Peter Seebach <peter.seebach@windriver.com>

--- ports/linux/xattr/guts/lsetxattr.c
+++ ports/linux/xattr/guts/lsetxattr.c
@@ -2,10 +2,10 @@
  * Copyright (c) 2014 Wind River Systems; see
  * guts/COPYRIGHT for information.
  *
- * int lsetxattr(const char *path, const char *name, const void *value, size_t size, int flags)
+ * int lsetxattr(const char *path, const char *name, const void *value, size_t size, int xflags)
  *	int rc = -1;
  */
-	rc = shared_setxattr(path, -1, name, value, size, flags);
+	rc = shared_setxattr(path, -1, name, value, size, xflags);
 
 /*	return rc;
  * }
