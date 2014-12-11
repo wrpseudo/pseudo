a151a269d3b1ad668c10f7b2bc3c8ee1c30ff931
Jeroen Hofstee <jeroen@myspectrum.nl>
2014-12-11 20:36:14 +0100

    make sure test are run as a unprivaliged user
    Since the whole idea to falsely report additional privalages
    make sure the user invoking the test doesn't alread have them.

--- run_tests.sh
+++ run_tests.sh
@@ -21,6 +21,15 @@ do
         esac
 done
 
+# make sure this is not run as root / group 0 / within pseudo, since
+# the fake uid / gid test will fail.
+uid=`id -u`
+gid=`id -g`
+if [ $uid -eq 0 -o $gid -eq 0 ]; then
+    echo "ERROR: the tests must not be run as a user with uid=0 (root) or gid=0 (root) or in pseudo itself!"
+    exit 1
+fi
+
 #The tests will be run on the build dir, not the installed versions
 #This requires to following be set properly.
 export PSEUDO_PREFIX=${PWD}
